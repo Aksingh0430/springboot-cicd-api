8d580a090f38050e77cbce466917d78c97c06bc3
Ashwani Singh <ak7668844@gmail.com>
2026-02-18 15:01:07 +0530

    Fix missing java.util.List import in integration test

--- src/test/java/com/devops/api/controller/ProductIntegrationTest.java
+++ src/test/java/com/devops/api/controller/ProductIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import java.util.List;
 
 import java.math.BigDecimal;
 
